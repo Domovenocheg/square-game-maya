baaf40229f0872bbdae1f523fa4f484596bd1d9b
Yegor Dyakovskiy <yegor.dyakovskiy@aiesec.net>
2023-05-03 15:47:24 +0600

    add few buttons

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //rgb(255,255,255)
-
+//change color of squares
 const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 const randomColor = () =>
   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
 
 const squares = document.querySelectorAll(".square");
-const container = document.querySelector('.container')
+const container = document.querySelector(".container");
 
 squares.forEach((square) =>
   square.addEventListener("click", function (e) {
@@ -15,7 +15,7 @@ squares.forEach((square) =>
 );
 
 const mess = document.querySelector(".mess");
-//chatGPT
+//change places of squares
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -29,4 +29,50 @@ function shuffleSquares() {
   shuffledSquaresArray.forEach((square) => container.appendChild(square));
 }
 
-mess.addEventListener('click', shuffleSquares)
+mess.addEventListener("click", shuffleSquares);
+
+//change size of squares
+
+const size = document.querySelector(".size");
+
+const changeShape = function () {
+  squares.forEach((square) => {
+    if (square.style.borderRadius === "0px") {
+      square.classList.remove("star-polygon");
+      square.style.borderRadius = "50px";
+      square.classList.remove("heart-polygon");
+    } else {
+      square.style.borderRadius = "0px";
+      square.classList.remove("star-polygon");
+      square.classList.remove("heart-polygon");
+    }
+  });
+};
+
+size.addEventListener("click", changeShape);
+//change to stars
+
+const star = document.querySelector(".star");
+
+const changeStar = function () {
+  squares.forEach((square) => {
+    square.style.borderRadius = "0px";
+    square.classList.remove("heart-polygon");
+    square.classList.toggle("star-polygon");
+  });
+};
+
+star.addEventListener("click", changeStar);
+
+//change to hearts
+const heart = document.querySelector(".heart");
+
+const changeheart = function () {
+  squares.forEach((square) => {
+    square.style.borderRadius = "0px";
+    square.classList.remove("star-polygon");
+    square.classList.toggle("heart-polygon");
+  });
+};
+
+heart.addEventListener("click", changeheart);
